f26d483dd65d37eaaed4aa2384bff691be363ce0
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-07-09 16:34:15 +0000

    to do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,44 +14,45 @@ const Home = () => {
 		event.preventDefault();
 
 		if (newTask) {
-			addTask(newTask);
-			setNewTask(''); 
+			setTasks([...tasks, newTask]);
+			setNewTask('');
 		}
 	};
 
-	let addTask = (newTask) => {
-		setTasks([tasks, newTask]);
-	};
-
 	let deleteTask = (index) => {
-		let newTasks = [tasks];
-		newTasks.splice(index, 1);
-		setTasks(newTasks);
+		let newTasks = [];
+		newTasks = tasks.filter((item, id) => {
+			if (index != id) {
+				return item
+			}
+		}
+		)
+		setTasks(newTasks)
 	};
 
-	let showDeleteIcon = (index) => {
-		// 
-	};
 
 	return (
-		<div>
-			<h1 text-secondary>To-Do List</h1>
-			<form class="form-floating" onSubmit={handleSubmit}>
-				<input class="form-control" type="text" id="floatingInputValue" placeholder="Add task" value={newTask} onChange={handleChange}/>
-				<label for="floatingInputValue">No tasks, add tasck...</label>
-			</form>
-			{tasks.length > 0 ? (
-				<ul>
-					{tasks.map((task, index) => (
-						<li key={index} onMouseEnter={() => showDeleteIcon(index)}>
-							{task}
-							{showDeleteIcon(index) && (
-								<span onClick={() => deleteTask(index)}>️</span>
-							)}
-						</li>
-					))}
-				</ul>
-			) : ("")}
+		<div className="conatiner-fluid p-4 mx-3 row g-3">
+			
+			<div class="col-sm-7 m-auto">
+				<h1 className="text-center text-secondary text-opacity-75 fw-bolder">To Do List</h1>
+				<form className="form-floating" onSubmit={handleSubmit}>
+					<input className="form-control" type="text" id="floatingInputValue" placeholder="Add task" value={newTask} onChange={handleChange} />
+					<label htmlFor="floatingInputValue" className="text-secondary">No tasks, add task...</label>
+				</form>
+				{tasks.length > 0 ? (
+					<ul className="list-group">
+						{tasks.map((task, index) => (
+							<li className="list-group-item item-li fw-semibold text-center" key={index}>
+								{task}
+
+								<button type="button" className="btn-close float-end" aria-label="Close" onClick={() => deleteTask(index)}>️</button>
+							</li>
+						))}
+					</ul>
+				) : ("")}
+				<p className="fst-italic font-monospace p-1 fs-6 text-secondary">To do tasks: {tasks.length}</p>
+			</div>
 		</div>
 	);
 }
